cbb8990825833f261e5173044d3ef1c88249606a
yuvaraj yerra <yuvaraj999@outlook.com>
2019-12-20 14:48:01 +0530

    First Commit

---
+++ springboot.buildingblocks/springboot.buildingblocks/src/main/java/com/stackify/restservices/springboot/buildingblocks/Application.java
@@ -0,0 +1,13 @@
+package com.stackify.restservices.springboot.buildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
